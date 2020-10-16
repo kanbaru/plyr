bbf2f448ee03a36d05764e0a4d386dcc93a030ea
Emma Lo <emma@iscute.dev>
2020-10-16 01:37:55 -0500

    built

--- dist/plyr.polyfilled.js
+++ dist/plyr.polyfilled.js
@@ -8742,6 +8742,8 @@ typeof navigator === "object" && (function (global, factory) {
           return value === 1 ? i18n.get('normal', this.config) : "".concat(value, "&times;");
 
         case 'quality':
+          if (t === 0) return "Auto";
+
           if (is$1.number(value)) {
             var label = i18n.get("qualityLabel.".concat(value), this.config);
 
@@ -8824,19 +8826,19 @@ typeof navigator === "object" && (function (global, factory) {
           if (!is.element(this.elements.settings.panels.loop)) {
               return;
           }
-           const options = ['start', 'end', 'all', 'reset'];
+            const options = ['start', 'end', 'all', 'reset'];
           const list = this.elements.settings.panels.loop.querySelector('[role="menu"]');
-           // Show the pane and tab
+            // Show the pane and tab
           toggleHidden(this.elements.settings.buttons.loop, false);
           toggleHidden(this.elements.settings.panels.loop, false);
-           // Toggle the pane and tab
+            // Toggle the pane and tab
           const toggle = !is.empty(this.loop.options);
           controls.toggleMenuButton.call(this, 'loop', toggle);
-           // Empty the menu
+            // Empty the menu
           emptyElement(list);
-           options.forEach(option => {
+            options.forEach(option => {
               const item = createElement('li');
-               const button = createElement(
+                const button = createElement(
                   'button',
                   extend(getAttributesFromSelector(this.config.selectors.buttons.loop), {
                       type: 'button',
@@ -8845,11 +8847,11 @@ typeof navigator === "object" && (function (global, factory) {
                   }),
                   i18n.get(option, this.config)
               );
-               if (['start', 'end'].includes(option)) {
+                if (['start', 'end'].includes(option)) {
                   const badge = controls.createBadge.call(this, '00:00');
                   button.appendChild(badge);
               }
-               item.appendChild(button);
+                item.appendChild(button);
               list.appendChild(item);
           });
       }, */
@@ -15456,7 +15458,7 @@ typeof navigator === "object" && (function (global, factory) {
         this.media.loop = toggle; // Set default to be a true toggle
 
         /* const type = ['start', 'end', 'all', 'none', 'toggle'].includes(input) ? input : 'toggle';
-             switch (type) {
+              switch (type) {
                 case 'start':
                     if (this.config.loop.end && this.config.loop.end <= this.currentTime) {
                         this.config.loop.end = null;
@@ -15464,20 +15466,20 @@ typeof navigator === "object" && (function (global, factory) {
                     this.config.loop.start = this.currentTime;
                     // this.config.loop.indicator.start = this.elements.display.played.value;
                     break;
-                 case 'end':
+                  case 'end':
                     if (this.config.loop.start >= this.currentTime) {
                         return this;
                     }
                     this.config.loop.end = this.currentTime;
                     // this.config.loop.indicator.end = this.elements.display.played.value;
                     break;
-                 case 'all':
+                  case 'all':
                     this.config.loop.start = 0;
                     this.config.loop.end = this.duration - 2;
                     this.config.loop.indicator.start = 0;
                     this.config.loop.indicator.end = 100;
                     break;
-                 case 'toggle':
+                  case 'toggle':
                     if (this.config.loop.active) {
                         this.config.loop.start = 0;
                         this.config.loop.end = null;
@@ -15486,7 +15488,7 @@ typeof navigator === "object" && (function (global, factory) {
                         this.config.loop.end = this.duration - 2;
                     }
                     break;
-                 default:
+                  default:
                     this.config.loop.start = 0;
                     this.config.loop.end = null;
                     break;
